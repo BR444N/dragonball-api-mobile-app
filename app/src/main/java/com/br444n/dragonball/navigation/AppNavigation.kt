b294cbe002ff713c04e898034082403d6ae9c1a7
BR444N <brios724@gmail.com>
2025-09-07 20:04:46 -0600

    :remove: Remove unused imports from AppNavigation and PlanetsScreen

--- app/src/main/java/com/br444n/dragonball/navigation/AppNavigation.kt
+++ app/src/main/java/com/br444n/dragonball/navigation/AppNavigation.kt
@@ -8,7 +8,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
